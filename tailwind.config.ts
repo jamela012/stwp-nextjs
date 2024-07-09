cd620d18b30ca4cecfcc400ec1b39050a709719c
Jamela <bairajamela@gmail.com>
2024-07-10 05:44:24 +0800

    Update global css and tailwind ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'logo-color': 'rgba(235,61,0,0.8)'
+      }
     },
   },
   plugins: [],
