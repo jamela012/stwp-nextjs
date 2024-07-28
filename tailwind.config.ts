22728971655cc102e8fdc9708ba3ea7f0845e240
Jamela <bairajamela@gmail.com>
2024-07-29 01:24:00 +0800

    Added design style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
@@ -15,9 +15,13 @@ const config: Config = {
       },
       colors: {
         'logo-color': 'rgba(235,61,0,0.8)'
+      },
+      height: {
+        '500': '500px',
       }
     },
   },
   plugins: [],
 };
+
 export default config;
