395fcc25fc102384331f63bdbc64307972f5f2b1
Jamela <bairajamela@gmail.com>
2024-08-12 03:56:33 +0800

    update admin inquiries

--- src/app/admin/inquiries/InquiryModal.tsx
+++ src/app/admin/inquiries/InquiryModal.tsx
@@ -1,5 +1,21 @@
 import { FC } from 'react';
 import { Inquiry } from './types';
+import { Timestamp } from 'firebase/firestore';
+
+function formatTimestampToDateString(timestamp: Timestamp) {
+    if (timestamp) {
+        const date = timestamp.toDate();
+        return date.toLocaleString('en-US', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: true,
+        });
+    }
+    return '';
+}
 
 interface InquiryModalProps {
     inquiry: Inquiry;
@@ -10,23 +26,58 @@ interface InquiryModalProps {
 const InquiryModal: FC<InquiryModalProps> = ({ inquiry, onClose, onDelete }) => {
     return (
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50">
-            <div className="bg-white p-4 rounded-lg shadow-lg max-w-lg w-full">
-                <h2 className="text-2xl font-semibold mb-4">Inquiry Details</h2>
+            <div className="bg-white p-6 rounded-lg shadow-lg max-w-3xl w-full h-auto overflow-y-auto">
+                <div className="border-b border-gray-200 pb-4 mb-4">
+                    <h2 className="text-xl font-semibold mb-2">{inquiry.name}: {inquiry.subject}</h2>
+                    <div className="text-gray-600 mb-2">
+                        <div className='flex'>
+                            <p className='mr-2 font-semibold'>Sender: </p>
+                            <div>
+                                <p className="font-medium">{inquiry.name}</p>
+                                <p>
+                                    <a 
+                                        href={`mailto:${inquiry.email}?subject=Inquiry%20Regarding%20${encodeURIComponent(inquiry.subject)}`} 
+                                        className="text-gray-600 hover:underline flex"
+                                    >
+                                        <p>{inquiry.email}</p>
+                                        <svg className="p-1 w-6 h-6 text-gray-600 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/>
+</svg>
+
+
+                                    </a>
+                                </p>
+
+                                <p>{inquiry.phone}</p>
+                            </div>
+                        </div>
+                        <div className='flex mt-2'>
+                            <p className='mr-2 font-semibold'>Subject: </p>
+                            <div>
+                                <p className="font-medium">{inquiry.subject}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="mb-4">
-                    <p><strong>Name:</strong> {inquiry.name}</p>
-                    <p><strong>Subject:</strong> {inquiry.subject}</p>
-                    <p><strong>Status:</strong> {inquiry.read ? 'Read' : 'Unread'}</p>
-                    {/* Add other inquiry details here */}
+                    <div className="text-sm bg-gray-100 p-4 rounded-md shadow-sm">
+                        <p className='text-justify'>
+                            Event: {inquiry.eventtype}
+                        </p>
+                        <p className='text-justify mb-1 border-b-2'>
+                            Date: {inquiry.eventdate}
+                        </p>
+                        <p className='text-justify leading-relaxed'>
+                            {inquiry.message}
+                        </p>
+                    </div>
+                </div>
+                <div className="text-sm text-gray-500 mb-4">
+                    <p><strong>Received on:</strong> {formatTimestampToDateString(inquiry.createdAt)}</p>
                 </div>
                 <div className="flex justify-end">
                     <button
-                        className="bg-red-500 text-white px-4 py-2 rounded mr-2"
-                        onClick={() => onDelete(inquiry.id)}
-                    >
-                        Delete
-                    </button>
-                    <button
-                        className="bg-gray-500 text-white px-4 py-2 rounded"
+                        className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
                         onClick={onClose}
                     >
                         Close
@@ -38,3 +89,4 @@ const InquiryModal: FC<InquiryModalProps> = ({ inquiry, onClose, onDelete }) => 
 };
 
 export default InquiryModal;
+    
--- src/app/admin/inquiries/page.tsx
+++ src/app/admin/inquiries/page.tsx
@@ -2,13 +2,31 @@
 
 import { useState, useEffect } from 'react';
 import { db } from '@/app/lib/firebase';
-import { collection, query, onSnapshot, doc, updateDoc, getDoc, getDocs, where, deleteDoc } from 'firebase/firestore';
+import { collection, query, onSnapshot, doc, updateDoc, deleteDoc, where, getDocs } from 'firebase/firestore';
 import { Inquiry } from './types';
 import InquiryModal from './InquiryModal';
+import { Timestamp } from 'firebase/firestore';
+import Swal from 'sweetalert2';
+
+function formatTimestampToDateString(timestamp: Timestamp) {
+    if (timestamp) {
+        const date = timestamp.toDate();
+        return date.toLocaleString('en-US', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: true,
+        });
+    }
+    return '';
+}
 
 export default function AdminInquiries() {
     const [inquiries, setInquiries] = useState<Inquiry[]>([]);
     const [selectedInquiry, setSelectedInquiry] = useState<Inquiry | null>(null);
+    const [selectedIds, setSelectedIds] = useState<Set<string>>(new Set());
 
     useEffect(() => {
         const q = query(collection(db, 'inquiries'));
@@ -16,9 +34,13 @@ export default function AdminInquiries() {
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             const inquiriesData: Inquiry[] = [];
             querySnapshot.forEach((docSnapshot) => {
-                const data = docSnapshot.data() as Omit<Inquiry, 'id'>; // Exclude 'id' from the data
+                const data = docSnapshot.data() as Omit<Inquiry, 'id'>;
                 inquiriesData.push({ id: docSnapshot.id, ...data });
             });
+
+            // Sort inquiries by createdAt in descending order
+            inquiriesData.sort((a, b) => (b.createdAt as Timestamp).toMillis() - (a.createdAt as Timestamp).toMillis());
+
             setInquiries(inquiriesData);
         });
 
@@ -26,22 +48,21 @@ export default function AdminInquiries() {
     }, []);
 
     const handleInquiryClick = async (inquiry: Inquiry) => {
-        console.log('Selected Inquiry ID:', inquiry.id); // Log the ID
+        // Do nothing if checkboxes are being selected
+        if (selectedIds.size > 0) return;
 
         try {
-            // Query to find the document with the matching field
             const q = query(collection(db, 'inquiries'), where('id', '==', inquiry.id));
             const querySnapshot = await getDocs(q);
 
             if (!querySnapshot.empty) {
-                const docSnap = querySnapshot.docs[0]; // Assuming there's only one document with the matching criteria
+                const docSnap = querySnapshot.docs[0];
                 const inquiryRef = doc(db, 'inquiries', docSnap.id);
 
                 if (!inquiry.read) {
                     await updateDoc(inquiryRef, { read: true });
-                    console.log('Inquiry marked as read:', inquiry.id);
                 }
-                setSelectedInquiry({ id: docSnap.id, ...docSnap.data() } as Inquiry);
+                setSelectedInquiry({ id: docSnap.data().id, ...docSnap.data() } as Inquiry);
             } else {
                 console.error('No such document!');
             }
@@ -50,40 +71,90 @@ export default function AdminInquiries() {
         }
     };
 
+    const handleCheckboxChange = (id: string, event: React.ChangeEvent<HTMLInputElement>) => {
+        event.stopPropagation(); // Prevent triggering row click
+        setSelectedIds((prevSelectedIds) => {
+            const newSelectedIds = new Set(prevSelectedIds);
+            if (newSelectedIds.has(id)) {
+                newSelectedIds.delete(id);
+            } else {
+                newSelectedIds.add(id);
+            }
+            return newSelectedIds;
+        });
+    };
+
+    const handleDeleteSelected = async () => {
+        const result = await Swal.fire({
+            title: 'Are you sure?',
+            text: 'You won\'t be able to revert this!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+        });
+
+        if (result.isConfirmed) {
+            try {
+                await Promise.all(
+                    Array.from(selectedIds).map(async (id) => {
+                        const q = query(collection(db, 'inquiries'), where('id', '==', id));
+                        const querySnapshot = await getDocs(q);
+
+                        if (!querySnapshot.empty) {
+                            const docRef = doc(db, 'inquiries', querySnapshot.docs[0].id);
+                            await deleteDoc(docRef);
+                        }
+                    })
+                );
+
+                setInquiries((prevInquiries) =>
+                    prevInquiries.filter((inquiry) => !selectedIds.has(inquiry.id))
+                );
+                setSelectedIds(new Set());
+                Swal.fire('Deleted!', 'Your selected inquiries have been deleted.', 'success');
+            } catch (error) {
+                console.error('Error deleting documents:', error);
+                Swal.fire('Error!', 'There was an error deleting the inquiries.', 'error');
+            }
+        }
+    };
+
     const handleCloseModal = () => {
         setSelectedInquiry(null);
     };
 
-    const handleDeleteInquiry = async (inquiryId: string) => {
-        console.log('Attempting to delete inquiry with ID:', inquiryId); // Log the ID
-    
-        try {
-            const docRef = doc(db, 'inquiries', inquiryId);
-            const docSnap = await getDoc(docRef);
-            
-            if (docSnap.exists()) {
-                await deleteDoc(docRef);
-                console.log('Inquiry deleted:', inquiryId);
-                // Update state to remove deleted inquiry
-                setInquiries((prevInquiries) => prevInquiries.filter((inquiry) => inquiry.id !== inquiryId));
-            } else {
-                console.error('No such document!');
-            }
-        } catch (error) {
-            console.error('Error deleting document:', error);
-        }
-    };
-    
-    
-    
-
     return (
         <div className="p-5">
             <h1 className="text-4xl font-semibold mb-5">Inquiries</h1>
+            <div className="flex justify-between items-center mb-4">
+                {selectedIds.size > 0 && (
+                    <button
+                        onClick={handleDeleteSelected}
+                        className="bg-red-500 text-sm text-white px-4 py-1 rounded hover:bg-red-600"
+                    >
+                        Delete
+                    </button>
+                )}
+            </div>
             <div className="bg-white shadow-md rounded-lg overflow-hidden">
                 <table className="min-w-full divide-y divide-gray-200">
                     <thead className="bg-gray-50">
                         <tr>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <input
+                                    type="checkbox"
+                                    className="form-checkbox"
+                                    checked={selectedIds.size === inquiries.length}
+                                    onChange={() => {
+                                        if (selectedIds.size === inquiries.length) {
+                                            setSelectedIds(new Set());
+                                        } else {
+                                            setSelectedIds(new Set(inquiries.map((inquiry) => inquiry.id)));
+                                        }
+                                    }}
+                                />
+                            </th>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Name
                             </th>
@@ -91,7 +162,7 @@ export default function AdminInquiries() {
                                 Subject
                             </th>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Status
+                                Received on
                             </th>
                         </tr>
                     </thead>
@@ -99,9 +170,23 @@ export default function AdminInquiries() {
                         {inquiries.map((inquiry) => (
                             <tr
                                 key={inquiry.id}
-                                onClick={() => handleInquiryClick(inquiry)}
+                                onClick={(e) => {
+                                    // Only open modal if clicked outside of checkbox
+                                    if (e.target instanceof HTMLInputElement && e.target.type === 'checkbox') {
+                                        return;
+                                    }
+                                    handleInquiryClick(inquiry);
+                                }}
                                 className={`cursor-pointer ${inquiry.read ? 'bg-gray-100' : 'bg-yellow-100'}`}
                             >
+                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    <input
+                                        type="checkbox"
+                                        className="form-checkbox"
+                                        checked={selectedIds.has(inquiry.id)}
+                                        onChange={(e) => handleCheckboxChange(inquiry.id, e)}
+                                    />
+                                </td>
                                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                     {inquiry.name}
                                 </td>
@@ -109,7 +194,7 @@ export default function AdminInquiries() {
                                     {inquiry.subject}
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {inquiry.read ? 'Read' : 'Unread'}
+                                    {formatTimestampToDateString(inquiry.createdAt)}
                                 </td>
                             </tr>
                         ))}
@@ -121,7 +206,7 @@ export default function AdminInquiries() {
                 <InquiryModal
                     inquiry={selectedInquiry}
                     onClose={handleCloseModal}
-                    onDelete={handleDeleteInquiry}
+                    onDelete={() => handleDeleteSelected()} // Single delete via modal
                 />
             )}
         </div>
